f72a3070ba78fad6bbeacdb744a6088b611d04e5
Kyle Hoernlein <khoernlein90@gmail.com>
2018-01-07 17:49:06 -0500

    added google auth

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3000;
+require("./services/passport");
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+require("./routes/authRoutes")(app);
 
 app.listen(PORT, () => {
   console.log("we're up and running");
